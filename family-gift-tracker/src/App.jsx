522bd6d35840480870773304d43ce02652cee9bb
Eli Bowlby <elijahbowlby@hotmail.com>
2024-12-09 18:53:35 -0600

    home

--- family-gift-tracker/src/App.jsx
+++ family-gift-tracker/src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// eslint-disable-next-line no-unused-vars
 import { useState } from "react";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/christmas-list-app">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
